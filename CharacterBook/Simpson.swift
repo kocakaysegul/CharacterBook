2a042a8c72d5fe67ec5257e105b607968fd139bf
Ayşegül Koçak <aysegulkocak.eru@gmail.com>
2022-11-03 19:27:57 +0300

    model created

---
+++ CharacterBook/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  CharacterBook
+//
+//  Created by Ayşegül Koçak on 3.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(name: String, job: String, image: UIImage) {
+        self.name = name
+        self.job = job
+        self.image = image
+    }
+    
+}
--- CharacterBook/ViewController.swift
+++ CharacterBook/ViewController.swift
@@ -10,21 +10,39 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpsons = [Simpson]() // We can reach it from everywhere
+    var chosenSimpson : Simpson?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        //simpson objects
+        let homer = Simpson(name: "Homer Simpson", job: "Nuclear Safety", image: UIImage(named: "homer")!)
+        let marge = Simpson(name: "Marge Simpson", job: "Housewife", image: UIImage(named: "marge")!)
+        let bart = Simpson(name: "Bart Simpson", job: "Student", image: UIImage(named: "bart")!)
+        let lisa = Simpson(name: "Lisa Simpson", job: "Student", image: UIImage(named: "lisa")!)
+        let maggie = Simpson(name: "Maggie Simpson", job: "Baby", image: UIImage(named: "maggie")!)
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(marge)
+        mySimpsons.append(bart)
+        mySimpsons.append(lisa)
+        mySimpsons.append(maggie)
+         
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return mySimpsons.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Homer Simpson"
+        cell.textLabel?.text = mySimpsons[indexPath.row].name
         return cell
     }
 
