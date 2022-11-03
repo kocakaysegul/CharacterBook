b0509174e765a3eda24bb0c724093baa84da306b
Ayşegül Koçak <aysegulkocak.eru@gmail.com>
2022-11-03 18:13:07 +0300

    assets uploaded

--- CharacterBook/DetailViewController.swift
+++ CharacterBook/DetailViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +21,5 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
